42ca9dd13c4cf7be138ba619f1e980b6ea4f6293
Alexandrefelix07 <alexandrefelix07@hotmail.com>
2020-09-17 01:28:56 -0300

    Adicionar arquivos de projeto.

---
+++ World_Felix_Informatica.DAL/Class/DalBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace World_Felix_Informatica.DAL.Class
+{
+    public class DalBase : DbContext
+    {
+        public DalBase(DbContextOptions<DalBase> options): base(options){}
+    }
+}
---
+++ World_Felix_Informatica/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace World_Felix_Informatica.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
