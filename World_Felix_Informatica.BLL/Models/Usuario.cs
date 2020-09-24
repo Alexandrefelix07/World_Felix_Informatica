23cefc9421e7e774b4fbd11fa602833fe2aa12f4
Alexandrefelix07 <alexandrefelix07@hotmail.com>
2020-09-23 21:14:05 -0300

    Configurando mapeamento das class

---
+++ World_Felix_Informatica.BLL/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace World_Felix_Informatica.BLL.Models
+{
+    public class Usuario : IdentityUser<string>
+    {
+        [Required(ErrorMessage = "O Campo {0} e Obrigatorio")]       
+        public string Nome { get; set; }
+        
+        [Required(ErrorMessage = "O Campo {0} e Obrigatorio")]
+        public string CPF { get; set; }
+        
+        [Required(ErrorMessage = "O Campo {0} e Obrigatorio")]
+        public DateTime DataNasc { get; set; }
+        
+        [Required(ErrorMessage = "O Campo {0} e Obrigatorio")]
+        public string Foto { get; set; }
+        
+        public bool PromeiroAcesso { get; set; }
+     
+
+    }
+}
---
+++ World_Felix_Informatica.DAL/Mapeamentos/UsuarioMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using World_Felix_Informatica.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace World_Felix_Informatica.DAL.Mapeamentos
+{
+    class UsuarioMap : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.Property(u => u.Id).ValueGeneratedOnAdd();
+            builder.Property(u => u.CPF).IsRequired().HasMaxLength(11);
+            builder.Property(u => u.DataNasc).IsRequired();
+            builder.Property(u => u.Foto).IsRequired();
+            builder.Property(u => u.Email).IsRequired();
+            builder.Property(u => u.PromeiroAcesso).IsRequired();
+
+            builder.ToTable("Usuarios");
+        }
+    }
+}
